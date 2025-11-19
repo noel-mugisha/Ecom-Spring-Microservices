a7cac88672127110abfbbcd3155327723bff853d
Noel <noelmugisha332@gmail.com>
2025-11-19 21:41:33 +0200

    feat: email sending for notifications

---
+++ services/notification-service/src/main/java/com/ecom/notificationservice/enums/EmailTemplates.java
@@ -0,0 +1,18 @@
+package com.ecom.notificationservice.enums;
+
+import lombok.Getter;
+
+public enum EmailTemplates {
+    PAYMENT_CONFIRMATION("payment-confirmation.html", "Payment successfully processed"),
+    ORDER_CONFIRMATION("order-confirmation.html", "Order successfully placed");
+
+    @Getter
+    private final String templateName;
+    @Getter
+    private final String subject;
+
+    EmailTemplates(String templateName, String subject) {
+        this.templateName = templateName;
+        this.subject = subject;
+    }
+}
--- services/notification-service/src/main/java/com/ecom/notificationservice/kafka/NotificationConsumer.java
+++ services/notification-service/src/main/java/com/ecom/notificationservice/kafka/NotificationConsumer.java
@@ -1,10 +1,12 @@
 package com.ecom.notificationservice.kafka;
 
+import com.ecom.notificationservice.email.EmailService;
 import com.ecom.notificationservice.entity.Notification;
 import com.ecom.notificationservice.enums.NotificationType;
 import com.ecom.notificationservice.kafka.dto.OrderConfirmation;
 import com.ecom.notificationservice.kafka.dto.PaymentConfirmation;
 import com.ecom.notificationservice.repository.NotificationRepository;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -15,26 +17,42 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class NotificationConsumer {
     private final NotificationRepository notificationRepository;
+    private final EmailService emailService;
 
     @KafkaListener(topics = "order-topic", groupId = "notification-service")
-    public void consumeOrderTopic(OrderConfirmation orderConfirmation) {
+    public void consumeOrderTopic(OrderConfirmation orderConfirmation) throws MessagingException {
         log.info("Consuming order confirmation from order: {}", orderConfirmation.orderReference());
         var notification = Notification.builder()
                 .notificationType(NotificationType.ORDER_CONFIRMATION)
                 .orderConfirmation(orderConfirmation)
                 .build();
         notificationRepository.save(notification);
-        // TODO :send confirmation email
+        // send confirmation email
+        var customerName = orderConfirmation.customer().firstName() + " " + orderConfirmation.customer().lastName();
+        emailService.sendOrderConfirmationEmail(
+                orderConfirmation.customer().email(),
+                customerName,
+                orderConfirmation.totalAmount(),
+                orderConfirmation.orderReference(),
+                orderConfirmation.products()
+        );
     }
 
     @KafkaListener(topics = "payment-topic", groupId = "notification-service")
-    public void consumePaymentTopic(PaymentConfirmation paymentConfirmation) {
+    public void consumePaymentTopic(PaymentConfirmation paymentConfirmation) throws MessagingException {
         log.info("Consuming payment confirmation from order: {}", paymentConfirmation.orderReference());
         var notification = Notification.builder()
                 .notificationType(NotificationType.PAYMENT_CONFIRMATION)
                 .paymentConfirmation(paymentConfirmation)
                 .build();
         notificationRepository.save(notification);
-        // TODO :send confirmation email
+        // send confirmation email
+        var customerName = paymentConfirmation.customerFirstName() + " " + paymentConfirmation.customerLastName();
+        emailService.sendPaymentSuccessEmail(
+                paymentConfirmation.customerEmail(),
+                customerName,
+                paymentConfirmation.amount(),
+                paymentConfirmation.orderReference()
+        );
     }
 }
